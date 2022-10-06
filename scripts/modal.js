5c55c0a2c1b724c3d398449d358136e76d541c45
Leonardo L Cunha <leonavidareal@gmail.com>
2022-10-06 17:29:41 -0300

    first try

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,11 @@
-/* Desenvolva sua lógica aqui */
+const openModal = () => {
+    const btnModal = document.querySelectorAll("[data-control-modal]")
+    btnModal.forEach((element) => {
+        element.addEventListener("click", () => {
+            let modalControl = element.getAttribute("data-control-modal")
+            document.getElementById(modalControl).classList.toggle("show-modal")
+        })
+    })
+
+}
+openModal()
